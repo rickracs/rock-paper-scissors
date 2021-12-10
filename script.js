c9105269dd62759f1874b590104a75950d0566f2
Razon <rracsko@gmail.com>
2021-12-10 21:30:03 +0100

    added basic UI

--- script.js
+++ script.js
@@ -2,39 +2,66 @@ let playerWins = 0;
 let computerWins = 0;
 
 function computerPlay() {
-    let pick = Math.floor(Math.random()*3)+1;
+    let pick = Math.floor(Math.random() * 3) + 1;
     if (pick == 1) return "rock"
     else if (pick == 2) return "paper"
     return "scissors";
 }
 
-function capitalize (pick) {
-    return pick.charAt(0).toUpperCase()+pick.slice(1);
+function capitalize(pick) {
+    return pick.charAt(0).toUpperCase() + pick.slice(1);
 }
 
-function playRound () {
-    let computerSelection=computerPlay();
-    let playerSelection=prompt().toLowerCase();
-
+function playRound(e) {
+    
+    let computerSelection = computerPlay();
+  let playerSelection = e.originalTarget.id;
+  
     if (playerSelection == computerSelection) return "It's a DRAW!"
-    else if (playerSelection=="rock" && computerSelection=="scissors" ||
-            playerSelection=="paper" && computerSelection=="rock" ||
-            playerSelection=="scissors" && computerSelection=="paper") {
-                playerWins++;
-            return ("You WON! "+ capitalize(playerSelection) + " beats "+capitalize(computerSelection) + ".");
-            }
-            else {
-                computerWins++;
-            return ("You LOST! "+ capitalize(computerSelection) + " beats "+capitalize(playerSelection) + ".");
-            }
-}
-
-function game() {
-    for (i=0; i<5; i++) {
-console.log(playRound());
+    else if (playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper") {
+        playerWins++;
+        //return
+        yourScore.textContent = "Your score: " + playerWins;
+        result.textContent = "You WON! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection) + ".";
     }
-console.log("Your score: " + playerWins);
-console.log("Computer Score: "+computerWins);
+    else {
+        computerWins++;
+        //return 
+        computerScore.textContent = "Computer score: "+computerWins;
+        result.textContent = "You LOST! " + capitalize(computerSelection) + " beats " + capitalize(playerSelection) + ".";
+    }
 }
 
-game();
+
+const yourScore = document.querySelector("#yourScore");
+const computerScore = document.querySelector("#computerScore");
+const result = document.querySelector(".lastGame");
+const buttons = document.querySelectorAll("button");
+
+//add listener for button click here, call function playRound
+buttons.forEach(btn => {
+    btn.addEventListener('click', playRound);
+});
+
+
+
+//add reset game after 5 wins
+
+
+
+
+
+
+
+/*
+function game() {
+    for (i = 0; i < 5; i++) {
+        console.log(playRound());
+    }
+    console.log("Your score: " + playerWins);
+    console.log("Computer Score: " + computerWins);
+}
+
+game();*/
